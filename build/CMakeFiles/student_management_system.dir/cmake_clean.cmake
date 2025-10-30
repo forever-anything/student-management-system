c7ded6678ca7900182754c82bbd72924c01d5306
Cherryblossom <w_han2006@outlook.com>
2025-10-30 22:28:34 +0800

    feat：实现用户界面和菜单系统 -创建主程序框架 -实现所有菜单功能 -添加用户输入处理

---
+++ build/CMakeFiles/student_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student_management_system.dir/link.d"
+  "CMakeFiles/student_management_system.dir/src/Database.cpp.o"
+  "CMakeFiles/student_management_system.dir/src/Database.cpp.o.d"
+  "CMakeFiles/student_management_system.dir/src/Student.cpp.o"
+  "CMakeFiles/student_management_system.dir/src/Student.cpp.o.d"
+  "CMakeFiles/student_management_system.dir/src/main.cpp.o"
+  "CMakeFiles/student_management_system.dir/src/main.cpp.o.d"
+  "student_management_system"
+  "student_management_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/student_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
